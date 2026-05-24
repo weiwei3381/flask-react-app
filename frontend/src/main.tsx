e545b25bd1cb22503012d55a5691e72308bfbefb
weiwei3381 <weiwei3381@live.com>
2026-05-24 22:06:14 +0800

    fixed: 解决旧版浏览器URL.parse不存在的bug

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/index.tsx'
+// 解决旧版浏览器URL.parse不存在, 使用core-js包并引入对应的polyfill
+import 'core-js/actual/url/parse'  
 
 
 createRoot(document.getElementById('root')!).render(
